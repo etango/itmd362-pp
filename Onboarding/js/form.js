8fa4a0eb1ed92ffb2f455d5ca938b304d76e9452
etang <etang315@gmail.com>
2016-02-03 22:46:37 -0600

    added submit and reset button

--- Onboarding/js/form.js
+++ Onboarding/js/form.js
@@ -10,14 +10,40 @@ y.type="email";
 y.name="Email";
 y.placeholder="example@gmail.com"
 
+var phone = document.createElement('input');
+phone.id="phone";
+phone.type="phone";
+phone.name="Phone-Number";
+phone.placeholder="(xxx)-xxx-xxxx";
+
 var labelemail = document.createElement('label');
 labelemail.id="email_name";
+var labelphone = document.createElement('label');
+labelemail.id="phone_number";
 
 var text1 = document.createTextNode("Email Address: ");
+var text2 = document.createTextNode("Phone Number: ");
+var text3 = document.createTextNode("Submit");
+var text4 = document.createTextNode("Reset");
 
+var lnbreak = document.createElement('br');
 
+var submit = document.createElement('button');
+submit.id = "submit";
+
+var reset = document.createElement('button');
+reset.id="reset";
 
 labelemail.appendChild(text1);
+labelphone.appendChild(text2);
+submit.appendChild(text3);
+reset.appendChild(text4);
 createform.appendChild(labelemail);
 createform.appendChild(y);
+createform.appendChild(lnbreak);
+createform.appendChild(labelphone);
+createform.appendChild(phone);
+createform.appendChild(lnbreak.cloneNode(true));
+createform.appendChild(submit);
+createform.appendChild(reset);
 x.appendChild(createform);
