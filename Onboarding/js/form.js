ae7a1aaa93ff3302a804347e70a817c243898df8
etang <etang315@gmail.com>
2016-02-04 22:32:55 -0600

    added menu with li and ul tag, using css to style the menu with hover

--- Onboarding/js/form.js
+++ Onboarding/js/form.js
@@ -2,24 +2,24 @@ var x = document.getElementById('sign-up');
 var createform = document.createElement('form');
 createform.id = "sign-up";
 createform.method = 'POST';
-createform.action='Submit';
+createform.action = 'Submit';
 
 var y = document.createElement('input');
-y.id="email";
-y.type="email";
-y.name="Email";
-y.placeholder="example@gmail.com"
+y.id = "email";
+y.type = "email";
+y.name = "Email";
+y.placeholder = "example@gmail.com";
 
 var phone = document.createElement('input');
-phone.id="phone";
-phone.type="phone";
-phone.name="Phone-Number";
-phone.placeholder="(xxx)-xxx-xxxx";
+phone.id = "phone";
+phone.type = "phone";
+phone.name = "Phone-Number";
+phone.placeholder = "(xxx)-xxx-xxxx";
 
 var labelemail = document.createElement('label');
-labelemail.id="email_name";
+labelemail.id = "email_name";
 var labelphone = document.createElement('label');
-labelemail.id="phone_number";
+labelemail.id = "phone_number";
 
 var text1 = document.createTextNode("Email Address: ");
 var text2 = document.createTextNode("Phone Number: ");
@@ -32,7 +32,7 @@ var submit = document.createElement('button');
 submit.id = "submit";
 
 var reset = document.createElement('button');
-reset.id="reset";
+reset.id = "reset";
 
 labelemail.appendChild(text1);
 labelphone.appendChild(text2);
