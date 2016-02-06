5553207edd6d443ac865500b2c11be304e6897c4
etango <etang315@gmail.com>
2016-02-05 19:38:30 -0600

    redid the submit and reset button.

--- Onboarding/js/form.js
+++ Onboarding/js/form.js
@@ -1,6 +1,6 @@
 var x = document.getElementById('sign-up');
 var createform = document.createElement('form');
-createform.id = "sign-up";
+createform.id = "sign-up-form";
 createform.method = 'POST';
 createform.action = 'Submit';
 
@@ -23,27 +23,22 @@ labelemail.id = "phone_number";
 
 var text1 = document.createTextNode("Email Address: ");
 var text2 = document.createTextNode("Phone Number: ");
-var text3 = document.createTextNode("Submit");
-var text4 = document.createTextNode("Reset");
+
 
 var lnbreak = document.createElement('br');
 
-var submit = document.createElement('button');
-submit.id = "submit";
 
-var reset = document.createElement('button');
-reset.id = "reset";
 
 labelemail.appendChild(text1);
 labelphone.appendChild(text2);
-submit.appendChild(text3);
-reset.appendChild(text4);
+
 createform.appendChild(labelemail);
 createform.appendChild(y);
 createform.appendChild(lnbreak);
+createform.appendChild(lnbreak.cloneNode(true));
 createform.appendChild(labelphone);
 createform.appendChild(phone);
 createform.appendChild(lnbreak.cloneNode(true));
-createform.appendChild(submit);
-createform.appendChild(reset);
+createform.appendChild(lnbreak.cloneNode(true));
+
 x.appendChild(createform);
